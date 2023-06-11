8c7b76721efab5af674d496f684746c5e1e658ad
ArjuArch <102756303+ArjuArch@users.noreply.github.com>
2023-06-11 20:21:37 +0530

    Update and rename Square_asterisk.c to Full_pyramid_asterisk.c

--- Full_pyramid_asterisk.c
+++ Full_pyramid_asterisk.c
@@ -1,3 +1,12 @@
+/*This code prints Full Pyramid pattern using Asterisk symbol
+INPUT:
+4
+OUTPUT:
+    *
+   * *
+  * * *
+ * * * *
+It's a basic pattern code that can be acheived with a basic knowledge in For loops*/
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
