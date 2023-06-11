e9b99129aa6e03ce5f9997b6aeafa80dda65feed
ArjuArch <102756303+ArjuArch@users.noreply.github.com>
2023-06-11 19:14:56 +0530

    Update Left_half_triangle.c

--- Left_half_triangle.c
+++ Left_half_triangle.c
@@ -1,3 +1,11 @@
+/*This code prints Left half triangle pattern using Asterisk symbol
+INPUT:
+3
+OUTPUT:
+* * *
+  * *
+    *
+It's a basic pattern code that can be acheived with a basic knowledge in For loops*/
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
