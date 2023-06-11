08bdc5a23b27c1b7e663a82a3f4202bb1f0ce622
ArjuArch <102756303+ArjuArch@users.noreply.github.com>
2023-06-11 20:36:39 +0530

    Add files via upload

---
+++ Hollow_square.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0||j==0||i==n-1||j==n-1){
+                printf("* ");
+            }else{
+                printf("  ");
+            }
+        }printf("\n");
+    }
+}/*This code prints Left half triangle pattern using Asterisk symbol
+INPUT:
+3
+OUTPUT:
+* * *
+  * *
+    *
+It's a basic pattern code that can be acheived with a basic knowledge in For loops*/
